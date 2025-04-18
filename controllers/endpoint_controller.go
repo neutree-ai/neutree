40e3483a5ce884b3b37d206d9a658c93cf6dd025
Yanzhen Yu <yanzhen@smartx.com>
2025-04-18 17:50:44 +0800

    feat: impl the endpoint controller with ray integration

--- controllers/endpoint_controller.go
+++ controllers/endpoint_controller.go
@@ -10,19 +10,23 @@ import (
 	"k8s.io/klog/v2"
 
 	v1 "github.com/neutree-ai/neutree/api/v1"
+	"github.com/neutree-ai/neutree/internal/orchestrator"
+	"github.com/neutree-ai/neutree/internal/registry"
 	"github.com/neutree-ai/neutree/pkg/storage"
 )
 
 type EndpointController struct {
 	baseController *BaseController
 
-	storage     storage.Storage
-	syncHandler func(endpoint *v1.Endpoint) error // Added syncHandler field
+	storage      storage.Storage
+	imageService registry.ImageService
+	syncHandler  func(endpoint *v1.Endpoint) error // Added syncHandler field
 }
 
 type EndpointControllerOption struct {
-	Storage storage.Storage
-	Workers int
+	ImageService registry.ImageService
+	Storage      storage.Storage
+	Workers      int
 }
 
 func NewEndpointController(option *EndpointControllerOption) (*EndpointController, error) {
@@ -94,8 +98,13 @@ func (c *EndpointController) sync(obj *v1.Endpoint) error {
 		}
 
 		klog.Info("Deleting endpoint " + obj.Metadata.Name)
+		err = c.cleanupEndpoint(obj)
+		if err != nil {
+			return errors.Wrapf(err, "failed to cleanup endpoint %s", obj.Metadata.Name)
+		}
+
 		// Update status to DELETED
-		err = c.updateStatus(obj, v1.EndpointPhaseDELETED, nil)
+		err = c.updateStatus(obj, c.formatStatus(v1.EndpointPhaseDELETED, nil))
 		if err != nil {
 			return errors.Wrapf(err, "failed to update endpoint %s status to DELETED", obj.Metadata.Name)
 		}
@@ -106,11 +115,56 @@ func (c *EndpointController) sync(obj *v1.Endpoint) error {
 	// Handle creation/update (when not deleting)
 	// If status is missing or PENDING, update it to RUNNING.
 	if obj.Status == nil || obj.Status.Phase == "" || obj.Status.Phase == v1.EndpointPhasePENDING {
-		klog.Infof("Endpoint %s is PENDING or has no status, updating to RUNNING", obj.Metadata.Name)
-		err = c.updateStatus(obj, v1.EndpointPhaseRUNNING, nil)
-
+		klog.Infof("Endpoint %s is PENDING or has no status, creating", obj.Metadata.Name)
+		status, err := c.createEndpoint(obj)
 		if err != nil {
-			return errors.Wrapf(err, "failed to update endpoint %s status to RUNNING", obj.Metadata.Name)
+			return errors.Wrapf(err, "failed to create endpoint %s", obj.Metadata.Name)
+		}
+
+		err = c.updateStatus(obj, status)
+		if err != nil {
+			return errors.Wrapf(err, "failed to update endpoint %s status", obj.Metadata.Name)
+		}
+
+		return nil
+	}
+
+	if obj.Status.Phase == v1.EndpointPhaseFAILED {
+		// TODO: check this strategy
+		klog.Infof("Endpoint %s is FAILED, re-creating", obj.Metadata.Name)
+
+		err = c.cleanupEndpoint(obj)
+		if err != nil {
+			return errors.Wrapf(err, "failed to cleanup endpoint %s", obj.Metadata.Name)
+		}
+
+		status, err := c.createEndpoint(obj)
+		if err != nil {
+			return errors.Wrapf(err, "failed to create endpoint %s", obj.Metadata.Name)
+		}
+
+		err = c.updateStatus(obj, status)
+		if err != nil {
+			return errors.Wrapf(err, "failed to update endpoint %s status", obj.Metadata.Name)
+		}
+
+		return nil
+	}
+
+	if obj.Status.Phase == v1.EndpointPhaseRUNNING {
+		klog.Infof("Endpoint %s is RUNNING, checking health", obj.Metadata.Name)
+		status, err := c.checkEndpointHealth(obj)
+		if err != nil {
+			return errors.Wrapf(err, "failed to check endpoint %s health", obj.Metadata.Name)
+		}
+
+		if status.Phase != v1.EndpointPhaseRUNNING {
+			klog.Infof("Endpoint %s is not RUNNING, updating status", obj.Metadata.Name)
+
+			err = c.updateStatus(obj, status)
+			if err != nil {
+				return errors.Wrapf(err, "failed to update endpoint %s status", obj.Metadata.Name)
+			}
 		}
 
 		return nil
@@ -119,7 +173,45 @@ func (c *EndpointController) sync(obj *v1.Endpoint) error {
 	return nil
 }
 
-func (c *EndpointController) updateStatus(obj *v1.Endpoint, phase v1.EndpointPhase, err error) error {
+func (c *EndpointController) createEndpoint(obj *v1.Endpoint) (*v1.EndpointStatus, error) {
+	o, err := c.getOrchestrator(obj)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to get orchestrator for endpoint %s", obj.Metadata.Name)
+	}
+
+	status, err := o.CreateEndpoint(obj)
+	if err != nil {
+		return status, errors.Wrapf(err, "failed to create endpoint %s", obj.Metadata.Name)
+	}
+
+	return status, nil
+}
+
+func (c *EndpointController) cleanupEndpoint(obj *v1.Endpoint) error {
+	return nil
+}
+
+func (c *EndpointController) checkEndpointHealth(obj *v1.Endpoint) (*v1.EndpointStatus, error) {
+	o, err := c.getOrchestrator(obj)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to get orchestrator for endpoint %s", obj.Metadata.Name)
+	}
+
+	status, err := o.GetEndpointStatus(obj)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to get endpoint %s status", obj.Metadata.Name)
+	}
+
+	return status, err
+}
+
+func (c *EndpointController) updateStatus(obj *v1.Endpoint, status *v1.EndpointStatus) error {
+	status.LastTransitionTime = time.Now().Format(time.RFC3339Nano)
+
+	return c.storage.UpdateEndpoint(strconv.Itoa(obj.ID), &v1.Endpoint{Status: status})
+}
+
+func (c *EndpointController) formatStatus(phase v1.EndpointPhase, err error) *v1.EndpointStatus {
 	newStatus := &v1.EndpointStatus{
 		LastTransitionTime: time.Now().Format(time.RFC3339Nano),
 		Phase:              phase,
@@ -130,5 +222,38 @@ func (c *EndpointController) updateStatus(obj *v1.Endpoint, phase v1.EndpointPha
 		newStatus.ErrorMessage = ""
 	}
 
-	return c.storage.UpdateEndpoint(strconv.Itoa(obj.ID), &v1.Endpoint{Status: newStatus})
+	return newStatus
+}
+
+func (c *EndpointController) getOrchestrator(obj *v1.Endpoint) (orchestrator.Orchestrator, error) {
+	cluster, err := c.storage.ListCluster(storage.ListOption{
+		Filters: []storage.Filter{
+			{
+				Column:   "metadata->name",
+				Operator: "eq",
+				Value:    obj.Spec.Cluster,
+			},
+			{
+				Column:   "metadata->workspace",
+				Operator: "eq",
+				Value:    obj.Metadata.Workspace,
+			},
+		},
+	})
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to get cluster %s", obj.Spec.Cluster)
+	}
+	if len(cluster) == 0 {
+		return nil, errors.New("cluster not found")
+	}
+
+	orchestrator, err := orchestrator.NewOrchestrator(orchestrator.Options{
+		Cluster:      &cluster[0],
+		Storage:      c.storage,
+		ImageService: c.imageService,
+	})
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to create orchestrator for cluster %s", cluster[0].Metadata.Name)
+	}
+	return orchestrator, nil
 }
