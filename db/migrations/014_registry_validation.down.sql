e782337d0ae2ed8cf13290fd1924f6aca7414585
xiangxin <xiangxin.ji@smartx.com>
2025-08-05 17:20:13 +0800

    feat: add validation trigger and function for model registry URL

---
+++ db/migrations/014_registry_validation.down.sql
@@ -0,0 +1,2 @@
+DROP TRIGGER IF EXISTS validate_model_registry_url_on_model_registry;
+DROP FUNCTION IF EXISTS api.validate_model_registry_url();
---
+++ db/migrations/014_registry_validation.up.sql
@@ -0,0 +1,23 @@
+CREATE OR REPLACE FUNCTION api.validate_model_registry_url()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF (NEW.spec).type = 'bentoml' AND ((NEW.spec).url IS NULL OR (NEW.spec).url = '') THEN
+        RAISE sqlstate 'PGRST'
+            USING message = '{"code": "10008","message": "spec.url is required","hint": "Provide file system path"}',
+            detail = '{"status": 400, "headers": {"X-Powered-By": "Neutree"}}';
+    END IF;
+
+    IF (NEW.spec).type = 'hugging-face' AND ((NEW.spec).url IS NULL OR (NEW.spec).url = '') THEN
+        RAISE sqlstate 'PGRST'
+            USING message = '{"code": "10008","message": "spec.url is required","hint": "Provide Hugging Face URL"}',
+            detail = '{"status": 400, "headers": {"X-Powered-By": "Neutree"}}';
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER validate_model_registry_url_on_model_registry
+    BEFORE INSERT OR UPDATE ON api.model_registry
+    FOR EACH ROW
+    EXECUTE FUNCTION api.validate_model_registry_url();
