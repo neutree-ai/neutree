da0d265d8acc4164c5c28f650b62db885dc45b19
xiangxin <xiangxin.ji@smartx.com>
2025-08-05 17:47:45 +0800

    feat: add validation triggers and functions for image registries

--- db/migrations/014_registry_validation.up.sql
+++ db/migrations/014_registry_validation.up.sql
@@ -1,3 +1,6 @@
+------------------------------------
+-- Model Registry URL Validation
+------------------------------------
 CREATE OR REPLACE FUNCTION api.validate_model_registry_url()
 RETURNS TRIGGER AS $$
 BEGIN
@@ -18,6 +21,50 @@ END;
 $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER validate_model_registry_url_on_model_registry
-    BEFORE INSERT OR UPDATE ON api.model_registry
+    BEFORE INSERT OR UPDATE ON api.model_registries
     FOR EACH ROW
     EXECUTE FUNCTION api.validate_model_registry_url();
+
+
+------------------------------------
+-- Image Registry URL Validation
+------------------------------------
+CREATE OR REPLACE FUNCTION api.validate_image_registry_url()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF (NEW.spec).url IS NULL OR (NEW.spec).url = '' THEN
+        RAISE sqlstate 'PGRST'
+            USING message = '{"code": "10009","message": "spec.url is required","hint": "Provide Image Registry URL"}',
+            detail = '{"status": 400, "headers": {"X-Powered-By": "Neutree"}}';
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER validate_image_registry_url_on_image_registry
+    BEFORE INSERT OR UPDATE ON api.image_registries
+    FOR EACH ROW
+    EXECUTE FUNCTION api.validate_image_registry_url();
+
+------------------------------------
+-- Image Registry Repo Validation
+------------------------------------
+CREATE OR REPLACE FUNCTION api.validate_image_registry_repo()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF (NEW.spec).repository IS NULL OR (NEW.spec).repository = '' THEN
+        RAISE sqlstate 'PGRST'
+            USING message = '{"code": "10010","message": "spec.repository is required","hint": "Provide Image Registry Repo"}',
+            detail = '{"status": 400, "headers": {"X-Powered-By": "Neutree"}}';
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+DROP TRIGGER IF EXISTS validate_image_registry_repo_on_image_registry ON api.image_registries;
+CREATE TRIGGER validate_image_registry_repo_on_image_registry
+    BEFORE INSERT OR UPDATE ON api.image_registries
+    FOR EACH ROW
+    EXECUTE FUNCTION api.validate_image_registry_repo();
