15c72168b3038905f7a1bae9e6a5d4d82d0d0d6e
Levi080513 <wei.huang@arcfra.com>
2026-01-10 15:46:15 +0800

    fix: sync api key usage always do nothing (#233)
    Co-authored-by: Levi080513 <weihuang@arcfra.com>

---
+++ db/migrations/046_fix-sync-api-key-usage.up.sql
@@ -0,0 +1,38 @@
+DROP FUNCTION IF EXISTS api.sync_api_key_usage();
+CREATE OR REPLACE FUNCTION api.sync_api_key_usage()
+RETURNS INTEGER
+SECURITY DEFINER
+AS $$
+DECLARE
+    v_count INTEGER := 0;
+    v_api_key RECORD;
+    v_total_usage BIGINT;
+BEGIN
+    FOR v_api_key IN
+        SELECT id, COALESCE((status).usage, 0) AS current_usage
+        FROM api.api_keys
+    LOOP
+        SELECT COALESCE(SUM((spec).total_usage), 0) INTO v_total_usage
+        FROM api.api_daily_usage
+        WHERE ((spec).api_key_id) = v_api_key.id;
+
+        IF v_total_usage != v_api_key.current_usage THEN
+            UPDATE api.api_keys
+            SET status = ROW(
+                (status).phase,
+                (status).last_transition_time,
+                (status).error_message,
+                (status).sk_value,
+                v_total_usage,
+                (status).last_used_at,
+                now()  -- Update last_sync_at
+            )::api.api_key_status
+            WHERE id = v_api_key.id;
+
+            v_count := v_count + 1;
+        END IF;
+    END LOOP;
+
+    RETURN v_count;
+END;
+$$ LANGUAGE plpgsql;
