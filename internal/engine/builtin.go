cf7c099064d0c36783c2c2c819625e883c86ace0
Levi080513 <wei.huang@arcfra.com>
2026-06-30 14:50:02 +0800

    feat: add vLLM v0.24.0 engine support

--- internal/engine/builtin.go
+++ internal/engine/builtin.go
@@ -26,6 +26,11 @@ func GetBuiltinEngines() ([]*v1.Engine, error) {
 		return nil, err
 	}
 
+	vllmV0_24_0EngineSchema, err := GetVLLMV0_24_0EngineSchema()
+	if err != nil {
+		return nil, err
+	}
+
 	sglangV0_5_10EngineSchema, err := GetSGLangV0_5_10EngineSchema()
 	if err != nil {
 		return nil, err
@@ -112,6 +117,21 @@ func GetBuiltinEngines() ([]*v1.Engine, error) {
 						DeployTemplate: map[string]map[string]string{
 							"kubernetes": {
 								"default": GetVLLMV0_17_1DeployTemplate(),
+							},
+						},
+					},
+					{
+						Version:      "v0.24.0",
+						ValuesSchema: vllmV0_24_0EngineSchema,
+						Images: map[string]*v1.EngineImage{
+							"nvidia_gpu": {
+								ImageName: "neutree/engine-vllm",
+								Tag:       "v0.24.0-ray2.53.0",
+							},
+						},
+						DeployTemplate: map[string]map[string]string{
+							"kubernetes": {
+								"default": GetVLLMV0_24_0DeployTemplate(),
 							},
 						},
 					},
