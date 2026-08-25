9eaee284bc918c8bf0e009e1f2ab58f9c69e5b77
Levi080513 <wei.huang@arcfra.com>
2026-08-25 17:11:00 +0800

    fix(nodeagent): retain app builder boundary (#594)

--- cmd/neutree-node-agent/main.go
+++ cmd/neutree-node-agent/main.go
@@ -7,23 +7,25 @@ import (
 	"os/signal"
 	"syscall"
 
+	nodeagentapp "github.com/neutree-ai/neutree/cmd/neutree-node-agent/app"
 	"github.com/neutree-ai/neutree/internal/version"
-	"github.com/neutree-ai/neutree/pkg/nodeagent"
 )
 
 func main() {
 	info := version.Get()
 	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 
-	err := nodeagent.Run(ctx, nodeagent.Config{
-		Args: os.Args[1:],
-		Build: nodeagent.BuildInfo{
+	application, err := nodeagentapp.NewBuilder().
+		WithArgs(os.Args[1:]).
+		WithBuildInfo(nodeagentapp.BuildInfo{
 			Version:   info.AppVersion,
 			GitCommit: info.GitCommit,
 			BuildTime: info.BuildTime,
-		},
-		Adapters: nodeagent.DefaultAdapters(),
-	})
+		}).
+		Build()
+	if err == nil {
+		err = application.Run(ctx)
+	}
 
 	cancel()
 
