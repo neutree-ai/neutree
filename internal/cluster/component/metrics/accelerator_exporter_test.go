b015789fcaace9fdfdd196695eadfb66c3ef743f
Levi080513 <wei.huang@arcfra.com>
2026-09-03 00:05:29 +0800

    test: remove managed exporter selector assertion

--- internal/cluster/component/metrics/accelerator_exporter_test.go
+++ internal/cluster/component/metrics/accelerator_exporter_test.go
@@ -4,31 +4,9 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 
 	v1 "github.com/neutree-ai/neutree/api/v1"
 )
-
-func TestManagedAcceleratorExportersPreservesProfileSelectors(t *testing.T) {
-	nvidiaSelector := map[string]string{"nvidia.com/gpu.present": "true"}
-	customSelector := map[string]string{"accelerator.example.com/custom": "true"}
-	plans := []metricsAcceleratorPlan{
-		{Exporter: &metricsAcceleratorExporter{
-			AcceleratorType: v1.AcceleratorTypeNVIDIAGPU.String(),
-			NodeSelector:    nvidiaSelector,
-		}},
-		{Exporter: &metricsAcceleratorExporter{
-			AcceleratorType: "custom_accelerator",
-			NodeSelector:    customSelector,
-		}},
-	}
-
-	exporters := managedAcceleratorExporters(plans)
-	require.Len(t, exporters, 2)
-	assert.Equal(t, nvidiaSelector, exporters[0].NodeSelector)
-	assert.Equal(t, nvidiaSelector, plans[0].Exporter.NodeSelector)
-	assert.Equal(t, customSelector, exporters[1].NodeSelector)
-}
 
 func TestBuildExporterConfigVolumesProjectsUnvalidatedProfileEntries(t *testing.T) {
 	configFileData, mounts, volumes, checksum := buildExporterConfigVolumes("test-exporter", []v1.AcceleratorExporterConfigFile{
